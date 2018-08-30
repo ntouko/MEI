5144dbfa5b206a91360d0958581325f65c1a6b70
naddyn <nadege.touko-toudjeu.1@ulaval.ca>
2018-08-30 17:55:50 -0400

    interface mei initialiser

---
+++ MEICodeSource/src/MEICodeSource/MEICodeSource.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MEICodeSource;
+
+/**
+ *
+ * @author 
+ */
+public class MEICodeSource {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
